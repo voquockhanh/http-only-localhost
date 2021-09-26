5f2510600d1444fc437dcf52f1c170eeb1434f16
khanh.vo <khanh.vo@grabtaxi.com>
2021-09-26 23:32:58 +0700

    finalize simple server

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# next-cookie-authz-api-sample
+# install
+- yarn install
+- yarn dev
+- browse: localhost:8080
+
+# testing nextjs rewrite 
+- config /etc/hosts following
+- 
+```
+127.0.0.1 {your-domain}.com
+127.0.0.1 api.{your-domain}.com
+```
+- run both nextjs and this test server
+
+# config nextjs rewrite
+- follow main page: https://nextjs.org/docs/api-reference/next.config.js/rewrites
+- config next.config.js following:
+```
+module.exports = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/api/:path*",
+          destination: "http://api.{your-domain}.com:8080/:path*",
+        },
+      ],
+    };
+  },
+};
+```
--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 var express = require("express");
-// var cors = require("cors");
 const cookieParser = require("cookie-parser");
 var app = express();
 var PORT = 8080;
 
 app.use(cookieParser());
-
-// app.use(cors());
-
-// app.use(function (req, res, next) {
-//     res.set('Access-Control-Allow-Origin', req.headers.origin); //req.headers.origin
-//     res.set('Access-Control-Allow-Credentials', 'true');
-//     // access-control-expose-headers allows JS in the browser to see headers other than the default 7
-//     res.set(
-//         'Access-Control-Expose-Headers',
-//         'date, etag, access-control-allow-origin, access-control-allow-credentials'
-//     );
-
-//     next()
-// })
 
 // Without middleware
 app.get("/debug", function (req, res) {
@@ -26,7 +11,7 @@ app.get("/debug", function (req, res) {
   res.json({ cookie: req.cookies });
 });
 
-app.get("/login", function (req, res) {
+app.post("/login", function (req, res) {
   console.log("/login route", process.env.NODE_ENV);
   const token = "*/*--=3432432$%^%$";
 
@@ -49,7 +34,7 @@ app.get("/validate", function (req, res) {
   res.status(200).json({ message: "success" });
 });
 
-app.get("/logout", function (req, res) {
+app.post("/logout", function (req, res) {
   console.log("/logout route", process.env.NODE_ENV);
   res.clearCookie("auth-token").status(200).json({ message: "success" });
 });
